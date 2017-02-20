817a6ac9622e06854cecfd93ad0baac1b13b6d18
helinjiang <me@helinjiang.com>
2017-02-20 18:15:03 +0800

    demo.js

--- 01.getting-started/demo.js
+++ 01.getting-started/demo.js
@@ -50,4 +50,68 @@ function runSpyDemo() {
     setNameSpy.restore();
 }
 
+/**
+ * 测试 sinon.stub
+ */
+function runStubDemo() {
+    // 模拟 jQuery.post
+    var jQuery = {
+        post: function (url, params, callback) {
+            console.log('--jQuery.post in! --', url, params);
+
+            setTimeout(function () {
+                console.log('--jQuery.post callback! --');
+                callback();
+            }, 1000)
+        }
+    };
+
+    // 创建变量保存我们期望看到的结果
+    var expectedUrl = '/users';
+    var expectedParams = {
+        first: 'Expected first name',
+        last: 'Expected last name'
+    };
+
+    // 创建将要作为参数的数据
+    var user = {
+        firstname: expectedParams.first,
+        lastname: expectedParams.last
+    };
+
+    // 我们的业务方法，新增用户
+    function saveUser(user, callback) {
+        jQuery.post('/users', {
+            first: user.firstname,
+            last: user.lastname
+        }, callback);
+    }
+
+    // 普通的测试
+    saveUser(user, function () {
+        console.log('Normal Test: saveUser success!');
+    });
+
+    // 我们会stub jQuery.post，这样就不用真正的发送请求
+    var post = sinon.stub(jQuery, 'post');
+    post.yields();
+
+    // 针对回调函数使用一个spy
+    var callback = sinon.spy();
+
+    // 使用 sinon 之后的调用测试
+    saveUser(user, callback);
+
+    // 恢复
+    post.restore();
+
+    // 判断：回调是否被调用了一次
+    console.log(callback.calledOnce); // true
+
+    // 判断：请求参数是否正确
+    console.log(post.calledWith(expectedUrl, expectedParams)); // true
+    console.log(post.calledWith('no' + expectedUrl, expectedParams)); // false
+}
+
 runSpyDemo();
+runStubDemo();
