d0cc45019af0ff6ab2b3a2a941dc01b25072af56
helinjiang <me@helinjiang.com>
2017-02-20 20:00:54 +0800

    utils

--- 02.using-mocha-chai/test/utils.test.js
+++ 02.using-mocha-chai/test/utils.test.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 var Utils = require('../src/utils');
 
 describe('No sinon - just how it works normally', function () {
-    it('Print: hello, Sinon', function () {
+    it('No sinon', function () {
         expect(Utils.sayHi('Sinon')).to.equal('hello, Sinon');
     });
 });
@@ -17,7 +17,26 @@ describe('Replace existing method', function () {
 
         it('replaces method with newHi', sinon.test(function () {
             this.stub(Utils, 'sayHi', newHi);
+
             expect(Utils.sayHi('Sinon')).to.equal('Sinon from new method');
         }));
     });
+});
+
+describe('Replace return', function () {
+
+    // Stubbing
+    it('change to return Mocha', sinon.test(function () {
+        this.stub(Utils, 'sayHi').returns('Mocha');
+
+        expect(Utils.sayHi('Sinon')).to.equal('Mocha');
+    }));
+
+    // Stubbing since behaviors are replaced
+    it('change to return Chai', sinon.test(function () {
+        this.stub(Utils, 'sayHi').returns('Chai');
+
+        expect(Utils.sayHi()).to.equal('Chai');
+    }));
+
 });
