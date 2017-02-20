327bfbf24b5e7ae84162ec63f1a1954d72e0562b
helinjiang <me@helinjiang.com>
2017-02-20 19:44:47 +0800

    teacher

---
+++ 02.using-mocha-chai/src/teacher.js
@@ -0,0 +1,5 @@
+function Teacher(name) {
+    this.name = name
+}
+
+module.exports = Teacher;
---
+++ 02.using-mocha-chai/test/teacher.test.js
@@ -0,0 +1,9 @@
+// var sinon = require('sinon');
+// var expect = require('chai').expect;
+
+//
+// describe('No sinon - just how it works normally', function(){
+//     it('just printing', function(){
+//         expect(Person.sayHi('Stanley')).to.equal('hey, Stanley');
+//     });
+// });
